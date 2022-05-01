d3b3710872639504760db6246984f956155e4ed0
willmclellarn <willmclellarn@gmail.com>
2022-05-01 11:43:50 -0600

    mern may 1

--- react-frontend-01-starting-setup/src/App.js
+++ react-frontend-01-starting-setup/src/App.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
+import Users from "./user/pages/Users";
 
 function App() {
-  return <h1>Let's start!</h1>;
+  return (
+    <Router>
+      <Route path="/users" exact="true">
+        <Users />
+      </Route>
+      <Redirect to="/" />
+    </Router>
+  );
 }
 
 export default App;
